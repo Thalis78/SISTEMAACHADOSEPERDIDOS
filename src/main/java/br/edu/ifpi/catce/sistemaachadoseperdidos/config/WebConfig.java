b6a6d32ebbdffc899273464118347ac80f707e2c
Thalisson <thalissonmoura138@gmail.com>
2025-07-07 21:17:38 -0300

    :recycle:refactor:Adicionando configurações de banco de dados (PostgreSQL) e pool de conexões

--- src/main/java/br/edu/ifpi/catce/sistemaachadoseperdidos/config/WebConfig.java
+++ src/main/java/br/edu/ifpi/catce/sistemaachadoseperdidos/config/WebConfig.java
@@ -5,6 +5,7 @@ import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -19,6 +20,7 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 @Configuration
 @ComponentScan(basePackageClasses = UserCotroller.class)
 @EnableWebMvc
+@EnableSpringDataWebSupport
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
@@ -33,13 +35,13 @@ public class WebConfig implements WebMvcConfigurer {
     public ISpringTemplateEngine templateEngine() {
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setEnableSpringELCompiler(true);
-        engine.setTemplateResolver(templateResolver());
+        engine.setTemplateResolver(iTemplateResolver());
         engine.addDialect(new LayoutDialect());
         return engine;
     }
 
     @Bean
-    public ITemplateResolver templateResolver() {
+    public ITemplateResolver iTemplateResolver() {
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
         resolver.setPrefix("classpath:/templates/");
         resolver.setSuffix(".html");
@@ -47,7 +49,6 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setCharacterEncoding("UTF-8");
         return resolver;
     }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
