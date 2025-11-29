83308f52ac04f2e1402a9ada3b67b7a71691aa5f
Thalisson <thalissonmoura138@gmail.com>
2025-11-29 15:22:07 -0300

    :sparkles:feat: reestrutura layout do sistema de achados e perdidos do IFPI, melhorando usabilidade e interface

--- src/main/java/br/edu/ifpi/catce/sistemaachadoseperdidos/repository/AlunoRepository.java
+++ src/main/java/br/edu/ifpi/catce/sistemaachadoseperdidos/repository/AlunoRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
 
-    @Query("SELECT a FROM AlunoModel a WHERE a.nome like :primeiroNome%")
-    List<AlunoModel> findByPrimeiroNome(@Param("primeiroNome") String primeiroNome);
+@Query("SELECT a FROM AlunoModel a WHERE LOWER(a.nome) LIKE LOWER(CONCAT(:primeiroNome, '%'))")
+List<AlunoModel> findByPrimeiroNome(@Param("primeiroNome") String primeiroNome);
 
 }
--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -1,15 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const passwordField = document.getElementById('password');
-    const toggleButton = document.querySelector('.password-toggle');
-    const eyeIcon = toggleButton.querySelector('.eye-icon');
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordField = document.getElementById("password");
+  const toggleButton = document.querySelector(".password-toggle");
+  const eyeIcon = toggleButton.querySelector(".eye-icon");
 
-    toggleButton.addEventListener('click', function () {
-        const isPassword = passwordField.type === 'password';
-        passwordField.type = isPassword ? 'text' : 'password';
+  toggleButton.addEventListener("click", function () {
+    const isPassword = passwordField.type === "password";
+    passwordField.type = isPassword ? "text" : "password";
 
-        eyeIcon.src = isPassword ? 'static/icons/eye.svg' : 'static/icons/eye-off.svg';
-    });
+    eyeIcon.src = isPassword
+      ? "static/icons/eye.svg"
+      : "static/icons/eye-off.svg";
+  });
 });
 function confirmarExclusao() {
-    window.location.href = '/confirmacao';
+  window.location.href = "/confirmacao";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toastMessage = document.getElementById("toast-message");
+
+  if (toastMessage && toastMessage.classList.contains("show-toast")) {
+    toastMessage.classList.remove("hidden");
+
+    setTimeout(() => {
+      toastMessage.style.transition = "opacity 0.5s ease, transform 0.5s ease";
+      toastMessage.style.opacity = "0";
+      toastMessage.style.transform = "translateY(-20px)";
+
+      setTimeout(() => {
+        toastMessage.classList.add("hidden");
+        toastMessage.classList.remove("show-toast");
+      }, 500);
+    }, 4000);
+  }
+});
